736f721d85b5579e3369fd87ec2f7b43e14801b3
Pinero99 <135345049+Pinero99@users.noreply.github.com>
2023-10-16 18:55:52 +0000

    On branch main   Changes to be committed: 	modified:   part1/courseinfo/src/App.jsx

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -13,8 +13,8 @@
 
 		return(
       <>
-      	<p>{props.part}</p>
-        <p>{props.exercise}</p>
+      	<p>{props.part.name}</p>
+        <p>{props.part.exercise}</p>
       </>
     )
   }
@@ -25,15 +25,12 @@
       <div>
     	  <Part 
         	part={props.part1} 
-          exercise={props.exercise1}
         />
     	  <Part 
         	part={props.part2} 
-          exercise={props.exercise2}
         />
      	  <Part 
         	part={props.part3} 
-          exercise={props.exercise3}
         />
       </div>
     )
@@ -41,36 +38,45 @@
 
   const Total= (props) =>{
     return(
-      <p>Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}</p>
+      <p>Number of exercises {props.part1.exercise + props.part2.exercise + props.part3.exercise}</p>
     )
   }
 
   const App= () =>{
+
     const course='Half Stack application developement'
-		const part1 ='Fundamentals of Reacts'
-    const part2 ='Using props to pass data'
-    const part3 ='state of component'
-    const exercise1 = 10
-    const exercise2 = 7
-    const exercise3 = 14
+		
+    const part1= {
+       name:'Fundamentals of Reacts',
+       exercise:10
+  	}
+
+    const part2={
+      name:'Using props to pass data',
+      exercise: 7
+    }
+
+    const part3={
+      name:'state of component',
+    	exercise: 14
+    }
+
   	return (
     	
       <div>
 
         <Header course={course}/>
         <Content 
-        	part1={part1} 
+        	part1={part1}
         	part2={part2}
         	part3={part3} 
-        	exercise1={exercise1} 
-        	exercise2={exercise2} 
-        	exercise3={exercise3}
+        	
         />
 
         <Total 
-        	exercise1={exercise1} 
-        	exercise2={exercise2} 
-        	exercise3={exercise3}
+        	part1={part1} 
+        	part2={part2} 
+        	part3={part3}
         />
         
       </div>
